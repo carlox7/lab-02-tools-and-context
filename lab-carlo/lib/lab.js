6b1f98f106860838a3984b2143b74dea84561f08
Carlo Santos <carlox7@gmail.com>
2017-04-19 09:34:01 -0700

    finished call and apply functions

---
+++ lab-carlo/lib/lab.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = exports = {};
+
+exports.Pokemon = function(name, type) {
+  this.name = name;
+  this.type = type;
+};
+
+exports.LegendaryPokemon = function(name, type) {
+  exports.Pokemon.call(this, name, type);
+  this.isLegendary = true;
+};
+
+exports.say = function(greet) {
+  console.log(this.name + '  says ' + '\"' + greet + '\"');
+};
+
+let pikachu = new exports.Pokemon('Pikachu', 'electric');
+console.log(pikachu);
+
+let mew = new exports.LegendaryPokemon('Mew', 'psychic');
+console.log(mew);
+
+exports.say.apply(pikachu, ['Pika pi Pikachu']);
+
+exports.say.apply(mew, ['Meewwwwwwwww!']);
